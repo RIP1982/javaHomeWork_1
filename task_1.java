71794fbd371e8066f7e3aec46586f812a62a3f47
RIP1982 <perovsky@list.ru>
2022-10-16 12:59:43 +0400

    Solution for task 1

---
+++ task_1.java
@@ -0,0 +1,24 @@
+//Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+import java.util.Scanner;
+public class task_1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Input n: ");
+        int n = Integer.parseInt(iScanner.nextLine());
+        System.out.println("1 + ... + n = " + sum(n)); 
+        System.out.println("1 * ... * n = " + multiply(n));
+    }
+    static int sum(int a) {
+        return a*(a + 1)/2;   
+    }
+    static int multiply(int a) {
+        int[] arr = new int [a];
+        int multiply = 1;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            multiply *= arr[i];
+        }
+        return multiply;
+    }
+
+}
